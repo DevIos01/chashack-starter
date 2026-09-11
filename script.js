75cdfe650da8c76c3138dc4020cabe7621c24a47
Shubham Panth <GamingCraft52@gmail.com>
2026-09-11 12:46:25 +0200

    Klickräknare för laserskott

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const laserKnapp = document.querySelector("#laser-knapp");
 const laserText = document.querySelector("#laser-text");
+const laserRaknare = document.querySelector("#laser-raknare");
+
+let antalSkott = 0;
 
 laserKnapp.addEventListener("click", function () {
     laserText.textContent = "PEW PEW! 🔴🔴 Konkurrensen är utplånad.";
+
+    antalSkott = antalSkott + 1;
+    laserRaknare.textContent = "Antal laserskott: " + antalSkott;
 });
